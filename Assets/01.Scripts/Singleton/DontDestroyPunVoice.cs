392b844c0c0c91988941b83680b837b18bafdfa4
Ritaera <154584398+Ritaera@users.noreply.github.com>
2024-08-10 12:52:55 +0900

    PunVoice 수정
    PunVoice 수정

---
+++ Assets/01.Scripts/Singleton/DontDestroyPunVoice.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroyPunVoice : MonoBehaviour
+{
+    private static DontDestroyPunVoice instance;
+
+    void Awake()
+    {
+        // 이미 인스턴스가 존재하면 이 오브젝트를 삭제
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        // 인스턴스가 존재하지 않으면 이 오브젝트를 인스턴스로 설정하고 삭제되지 않도록 설정
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+}
